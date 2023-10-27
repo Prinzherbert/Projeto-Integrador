ca71cc02fcf31a379f430591d703d48f8051eaa4
Prinzherbert <pikachu.dat40@gmail.com>
2023-10-27 14:50:03 -0300

    working routes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require('http')
 const url = require('url')
 const mysql = require('mysql')
+const express = require('express'); 
+const app = express(); 
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -14,6 +16,34 @@ con.connect(function(err) {
   console.log("Connected!")
 })
 
+app.get('/', function(req, res) { 
+  res.json({ 
+    body: "Hello World!"
+  }); 
+}); 
+
+app.get('/parts/models', function(req, res) { 
+  con.query("SELECT * FROM acme.part_models", function (err, result) {
+    if (err) throw err
+    res.json(result)
+    console.log(result)
+  });
+});
+  
+app.get('/parts/units', function(req, res) { 
+  con.query("SELECT * FROM acme.part_units", function (err, result) {
+    if (err) throw err
+    res.json(result)
+    console.log(result)
+  });
+}); 
+  
+const PORT = process.env.PORT || 3000
+app.listen(PORT, function(req, res) { 
+  console.log(`Server is running on port ${PORT}`); 
+});
+
+/**
 const server = http.createServer((req, res) => {
   const reqUrl = url.parse(req.url).pathname
   if(reqUrl == '/') {
@@ -41,3 +71,4 @@ const PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 });
+*/
